9475d1b6f47b2e1590a3574f82adabacba3c969e
M0H1K4N <nejer911@gmail.com>
2023-11-23 19:05:55 +0400

    Server

--- event.js
+++ event.js
@@ -1,14 +1,13 @@
 const EventEmitter = require("events");
+const http = require("http");
 
-
-class Sales extends EventEmitter{
-    constructor(){
-        super();
-    }
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
 }
 
 const myEmitter = new Sales();
-
 
 myEmitter.on("newSale", () => {
   console.log("There was a new sale!");
@@ -23,3 +22,24 @@ myEmitter.on("newSale", (stock) => {
 });
 
 myEmitter.emit("newSale", 9);
+//event listeners around
+////////////////////////////////////////
+
+/// How to create server? || See down there
+TODO: var server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request recieved");
+  console.log(req.url)
+  res.end("Request recieved");
+});
+server.on("request", (req, res) => {
+  console.log("Another request");
+});
+server.on("close", () => {
+  console.log("Server closed! ");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("waiting for request....");
+});
