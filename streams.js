bcd96aa8a28313b17ac629254acbf169858a428d
M0H1K4N <nejer911@gmail.com>
2023-11-23 21:10:54 +0400

    solution3

--- streams.js
+++ streams.js
@@ -8,15 +8,24 @@ server.on("request", (req, res) => {
   //     res.end(data);
   //   });
 
-  // Solution 2: Streams
+  // Solution 2: Streams || winas jobia
 
+  //   const readable = fs.createReadStream("test-file.txt");
+  //   readable.on("data", (chunk) => {
+  //     res.write(chunk);
+  //   });
+  //   readable.on("end", () => {
+  //     res.end();
+  //   });
+  //   readable.on("error", (err) => {
+  //     console.log(err);
+  //     res.statusCode = 500;
+  //     res.end("File not found");
+  //   });
+
+  // Solution 3 ||ყველაზე პატარა და ძლიერი კოდი <3
   const readable = fs.createReadStream("test-file.txt");
-  readable.on("data", (chunk) => {
-    res.write(chunk);
-  });
-  readable.on('end', () => {
-    res.end();
-  })
+  readable.pipe(res);
 });
 
 server.listen(8000, "127.0.0.1", () => {
