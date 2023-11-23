20fe0e3c4008d2648aaee8898fb4a41c93e668ff
M0H1K4N <nejer911@gmail.com>
2023-11-23 20:58:39 +0400

    ADd solution 2

--- streams.js
+++ streams.js
@@ -2,9 +2,23 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
-  // Solution 1
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  // Solution 1  * kaia mara bevr iuzers ver udzlebs
+  //   fs.readFile("test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+
+  // Solution 2: Streams
+
+  const readable = fs.createReadStream("test-file.txt");
+  readable.on("data", (chunk) => {
+    res.write(chunk);
   });
+  readable.on('end', () => {
+    res.end();
+  })
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listenning...");
 });
