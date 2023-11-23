ddaf9cab783a34dd77db2836711255352b9a93b2
M0H1K4N <nejer911@gmail.com>
2023-11-23 14:42:15 +0400

    nextTick

--- event.loop.js
+++ event.loop.js
@@ -5,6 +5,13 @@ setImmediate(() => console.log("immediate finished"));
 
 fs.readFile("test-file.txt", () => {
   console.log("I/O finished");
+  console.log("-------------------------");
+
+  setTimeout(() => console.log("Timer 2 finished"), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  setImmediate(() => console.log("immediate 2 finished"));
+
+  process.nextTick(() => console.log("Process.nextTick"));
 });
 
 console.log("Hello from the too level code");
